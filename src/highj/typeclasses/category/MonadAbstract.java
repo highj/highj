11883533c540802d31bc04d92f7f818fddd4ea40
danielgronau@gmail.com <danielgronau@gmail.com@bdcd8b68-9dcc-aa74-f1b9-b4da3e61a8cc>
2011-05-25 21:58:54 +0000

    - added Comonads: ATTENTION ComonadAbstract is not ready yet - Monad changes (map and fold methods to functions)

--- src/highj/typeclasses/category/MonadAbstract.java
+++ src/highj/typeclasses/category/MonadAbstract.java
@@ -18,8 +18,6 @@ import highj.data.ListOf;
  * @author DGronau
  */
 public abstract class MonadAbstract<Ctor> extends ApplicativeAbstract<Ctor> implements Monad<Ctor> {
-
-    
     @Override
     // (>>=) (Control.Monad)
     public abstract <A, B> _<Ctor, B> bind(_<Ctor, A> nestedA, F<A, _<Ctor, B>> fn);
@@ -29,11 +27,11 @@ public abstract class MonadAbstract<Ctor> extends ApplicativeAbstract<Ctor> impl
     public <A, B> _<Ctor, B> semicolon(_<Ctor, A> nestedA, _<Ctor, B> nestedB) {
         return bind(nestedA, Function.<A, _<Ctor, B>>constant().f(nestedB));
     }
-    
+
     @Override
     // join (Control.Monad)
-    public <A> _<Ctor,A> join(_<Ctor, _<Ctor,A>> nestedNestedA) {
-        return bind(nestedNestedA, Function.<_<Ctor,A>>identity());
+    public <A> _<Ctor, A> join(_<Ctor, _<Ctor, A>> nestedNestedA) {
+        return bind(nestedNestedA, Function.<_<Ctor, A>>identity());
     }
 
     @Override
@@ -45,26 +43,26 @@ public abstract class MonadAbstract<Ctor> extends ApplicativeAbstract<Ctor> impl
     @Override
     // sequence (Control.Monad)
     public <A> _<Ctor, _<ListOf, A>> sequence(_<ListOf, _<Ctor, A>> ms) {
-       return fmap(new F<List<A>,_<ListOf, A>>(){
+        return fmap(new F<List<A>, _<ListOf, A>>() {
             @Override
             public _<ListOf, A> f(List<A> a) {
                 return ListOf.wrap(a);
             }
-         },sequenceFlat(ListOf.unwrap(ms)));
+        }, sequenceFlat(ListOf.unwrap(ms)));
     }
-    
+
     @Override
     // "flat" version of sequence
     public <A> _<Ctor, List<A>> sequenceFlat(List<_<Ctor, A>> list) {
-      //  sequence ms = foldr k (return []) ms
-      //      where k m m' = do { x <- m; xs <- m'; return (x:xs) }
-      return list.foldRight(new F2<_<Ctor, A>,_<Ctor, List<A>>,_<Ctor, List<A>>>(){
+        //  sequence ms = foldr k (return []) ms
+        //      where k m m' = do { x <- m; xs <- m'; return (x:xs) }
+        return list.foldRight(new F2<_<Ctor, A>, _<Ctor, List<A>>, _<Ctor, List<A>>>() {
             @Override
             public _<Ctor, List<A>> f(_<Ctor, A> m, final _<Ctor, List<A>> m_) {
-                return bind(m, new F<A, _<Ctor, List<A>>>(){
+                return bind(m, new F<A, _<Ctor, List<A>>>() {
                     @Override
                     public _<Ctor, List<A>> f(final A a) {
-                        return MonadAbstract.this.bind(m_, new F<List<A>, _<Ctor, List<A>>>(){
+                        return MonadAbstract.this.bind(m_, new F<List<A>, _<Ctor, List<A>>>() {
                             @Override
                             public _<Ctor, List<A>> f(List<A> listA) {
                                 return returnM(listA.cons(a));
@@ -75,52 +73,72 @@ public abstract class MonadAbstract<Ctor> extends ApplicativeAbstract<Ctor> impl
             }
         }, returnM(List.<A>nil()));
     }
-    
+
     // sequence_ (Control.Monad) 
     @Override
-    public <A> _<Ctor, Unit> sequence_(_<ListOf, _<Ctor, A>> list){
+    public <A> _<Ctor, Unit> sequence_(_<ListOf, _<Ctor, A>> list) {
         return sequence_Flat(ListOf.unwrap(list));
     }
-    
+
     // "flat" version of sequence_ 
     @Override
     public <A> _<Ctor, Unit> sequence_Flat(List<_<Ctor, A>> list) {
-        return list.foldRight(new F2<_<Ctor, A>, _<Ctor, Unit>, _<Ctor, Unit>>(){
+        return list.foldRight(new F2<_<Ctor, A>, _<Ctor, Unit>, _<Ctor, Unit>>() {
             @Override
             public _<Ctor, Unit> f(_<Ctor, A> a, _<Ctor, Unit> b) {
-                return semicolon(a,b);
+                return semicolon(a, b);
             }
         }, returnM(Unit.unit()));
     }
 
     @Override
     // mapM (Control.Monad)
-    public <A,B> _<Ctor,_<ListOf,B>> mapM(F<A,_<Ctor, B>> fn, _<ListOf,A> list) {
-        return sequence(ListMonadPlus.getInstance().fmap(fn,list));
+    public <A, B> F<_<ListOf, A>, _<Ctor, _<ListOf, B>>> mapM(final F<A, _<Ctor, B>> fn) {
+        return new F<_<ListOf, A>, _<Ctor, _<ListOf, B>>>() {
+            @Override
+            public _<Ctor, _<ListOf, B>> f(_<ListOf, A> list) {
+                return sequence(ListMonadPlus.getInstance().fmap(fn, list));
+            }
+        };
     }
 
     @Override
     // "flat" version of mapM
-    public <A,B> _<Ctor, List<B>> mapMFlat(F<A,_<Ctor, B>> fn, List<A> list) {
-        return sequenceFlat(list.map(fn));
+    public <A, B> F<List<A>, _<Ctor, List<B>>> mapMFlat(final F<A, _<Ctor, B>> fn) {
+        return new F<List<A>, _<Ctor, List<B>>>() {
+            @Override
+            public _<Ctor, List<B>> f(List<A> list) {
+                return sequenceFlat(list.map(fn));
+            }
+        };
     }
-    
+
     @Override
     // mapM_ (Control.Monad)
-    public <A, B> _<Ctor, Unit> mapM_(F<A, _<Ctor, B>> fn, _<ListOf, A> list) {
-        return mapM_Flat(fn, ListOf.unwrap(list));         
+    public <A, B> F<_<ListOf, A>, _<Ctor, Unit>> mapM_(final F<A, _<Ctor, B>> fn) {
+        return new F<_<ListOf, A>, _<Ctor, Unit>>() {
+            @Override
+            public _<Ctor, Unit> f(_<ListOf, A> list) {
+                return sequence_(ListMonadPlus.getInstance().fmap(fn, list));
+            }
+        };
     }
-    
+
     @Override
     // "flat" version of mapM_
-    public <A, B> _<Ctor, Unit> mapM_Flat(F<A, _<Ctor, B>> fn, List<A> list) {
-        return sequence_Flat(list.map(fn));
+    public <A, B> F<List<A>, _<Ctor, Unit>> mapM_Flat(final F<A, _<Ctor, B>> fn) {
+        return new F<List<A>, _<Ctor, Unit>>() {
+            @Override
+            public _<Ctor, Unit> f(List<A> list) {
+                return sequence_Flat(list.map(fn));
+            }
+        };
     }
-   
+
     @Override
     // (>=>) (Control.Monad) left-to-right Kleisli composition of monads
-    public <A,B,C> F<A, _<Ctor,C>> kleisli(final F<A, _<Ctor,B>> f, final F<B, _<Ctor,C>> g) {
-        return new F<A, _<Ctor,C>>() {
+    public <A, B, C> F<A, _<Ctor, C>> kleisli(final F<A, _<Ctor, B>> f, final F<B, _<Ctor, C>> g) {
+        return new F<A, _<Ctor, C>>() {
             @Override
             public _<Ctor, C> f(A a) {
                 return MonadAbstract.this.bind(f.f(a), g);
@@ -133,59 +151,121 @@ public abstract class MonadAbstract<Ctor> extends ApplicativeAbstract<Ctor> impl
     public <A, B> _<Ctor, B> ap(_<Ctor, F<A, B>> nestedFn, _<Ctor, A> nestedA) {
         return star(nestedFn, nestedA);
     }
-    
+
     @Override
     //foldM (Control.Monad) 
-    public <A, B> _<Ctor, A> foldM(F<A,F<B,_<Ctor,A>>> fn, A a, _<ListOf, B> listB){
-        return foldMFlat(Function.uncurryF2(fn), a, ListOf.unwrap(listB));
-    }
-    @Override
-    //"flat" version of foldM 
-    public <A, B> _<Ctor, A> foldMFlat(final F2<A,B,_<Ctor,A>> fn, A a, List<B> listB) {
-        _<Ctor, A> result = returnM(a);
-        final B[] b = (B[]) new Object[1];
-        F<A,_<Ctor,A>> fnBind = new F<A,_<Ctor,A>>(){
+    public <A, B> F2<A, _<ListOf, B>, _<Ctor, A>> foldM(final F<A, F<B, _<Ctor, A>>> fn) {
+        return new F2<A, _<ListOf, B>, _<Ctor, A>>() {
             @Override
-            public _<Ctor, A> f(A a) {
-                return fn.f(a, b[0]);
+            public _<Ctor, A> f(A a, _<ListOf, B> wrappedListB) {
+                _<Ctor, A> result = returnM(a);
+                final B[] b = (B[]) new Object[1];
+                F<A, _<Ctor, A>> fnBind = new F<A, _<Ctor, A>>() {
+                    @Override
+                    public _<Ctor, A> f(A a) {
+                        return fn.f(a).f(b[0]);
+                    }
+                };
+                List<B> listB = ListOf.unwrap(wrappedListB);
+                while (listB.isNotEmpty()) {
+                    b[0] = listB.head();
+                    listB = listB.tail();
+                    result = bind(result, fnBind);
+                }
+                return result;
             }
         };
-        while(listB.isNotEmpty()) {
-           b[0] = listB.head();
-           listB = listB.tail();
-           result = bind(result, fnBind); 
-        }  
-        return result;
     }
-    
+
+    @Override
+    //"flat" version of foldM 
+    public <A, B> F2<A, List<B>, _<Ctor, A>> foldMFlat(final F2<A, B, _<Ctor, A>> fn) {
+        return new F2<A, List<B>, _<Ctor, A>>() {
+            @Override
+            public _<Ctor, A> f(A a, List<B> listB) {
+                _<Ctor, A> result = returnM(a);
+                final B[] b = (B[]) new Object[1];
+                F<A, _<Ctor, A>> fnBind = new F<A, _<Ctor, A>>() {
+                    @Override
+                    public _<Ctor, A> f(A a) {
+                        return fn.f(a, b[0]);
+                    }
+                };
+                while (listB.isNotEmpty()) {
+                    b[0] = listB.head();
+                    listB = listB.tail();
+                    result = bind(result, fnBind);
+                }
+                return result;
+            }
+        };
+    }
+
     @Override
     //foldM_ (Control.Monad)
-    public <A, B> _<Ctor, Unit> foldM_(F<A,F<B,_<Ctor,A>>> fn, A a, _<ListOf, B> listB) {
-        return semicolon(foldM(fn, a, listB), returnM(Unit.unit()));
+    public <A, B> F2<A, _<ListOf, B>,_<Ctor, Unit>> foldM_(final F<A,F<B,_<Ctor,A>>> fn) {
+        return new F2<A, _<ListOf, B>,_<Ctor, Unit>>() {
+            @Override
+            public _<Ctor, Unit> f(A a, _<ListOf, B> wrappedListB) {
+                _<Ctor, A> result = returnM(a);
+                final B[] b = (B[]) new Object[1];
+                F<A, _<Ctor, A>> fnBind = new F<A, _<Ctor, A>>() {
+                    @Override
+                    public _<Ctor, A> f(A a) {
+                        return fn.f(a).f(b[0]);
+                    }
+                };
+                List<B> listB = ListOf.unwrap(wrappedListB);
+                while (listB.isNotEmpty()) {
+                    b[0] = listB.head();
+                    listB = listB.tail();
+                    result = bind(result, fnBind);
+                }
+                return returnM(Unit.unit());
+            }
+        };
     }
 
     @Override
     //"flat" version of foldM_ 
-    public <A, B> _<Ctor, Unit> foldM_Flat(F2<A, B,_<Ctor,A>> fn, A a, List<B> listB) {
-        return semicolon(foldMFlat(fn, a, listB), returnM(Unit.unit()));
+    public <A, B> F2<A,List<B> ,_<Ctor, Unit>> foldM_Flat(final F2<A, B,_<Ctor,A>> fn) {
+        return new F2<A, List<B>, _<Ctor, Unit>>() {
+            @Override
+            public _<Ctor, Unit> f(A a, List<B> listB) {
+                _<Ctor, A> result = returnM(a);
+                final B[] b = (B[]) new Object[1];
+                F<A, _<Ctor, A>> fnBind = new F<A, _<Ctor, A>>() {
+                    @Override
+                    public _<Ctor, A> f(A a) {
+                        return fn.f(a, b[0]);
+                    }
+                };
+                while (listB.isNotEmpty()) {
+                    b[0] = listB.head();
+                    listB = listB.tail();
+                    result = bind(result, fnBind);
+                }
+                return returnM(Unit.unit());
+            }
+        };
     }
-    
+
     //replicateM (Control.Monad)
     @Override
-    public <A> _<Ctor,_<ListOf,A>> replicateM(int n, _<Ctor, A> nestedA){
-         //replicateM n x    = sequence (replicate n x)
+    public <A> _<Ctor, _<ListOf, A>> replicateM(int n, _<Ctor, A> nestedA) {
+        //replicateM n x    = sequence (replicate n x)
         return sequence(ListOf.wrap(List.replicate(n, nestedA)));
     }
-    
+
     //"flat" version of replicateM 
     @Override
-    public <A> _<Ctor,List<A>> replicateMFlat(int n, _<Ctor, A> nestedA){
-        return sequenceFlat(List.replicate(n, nestedA));   
+    public <A> _<Ctor, List<A>> replicateMFlat(int n, _<Ctor, A> nestedA) {
+        return sequenceFlat(List.replicate(n, nestedA));
     }
-    
+
     //replicateM_ (Control.Monad)
     @Override
-    public <A> _<Ctor, Unit> replicateM_(int n, _<Ctor, A> nestedA){
+    public <A> _<Ctor, Unit> replicateM_(int n, _<Ctor, A> nestedA) {
         return sequence_Flat(List.replicate(n, nestedA));
     }
 }
