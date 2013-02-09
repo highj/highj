05c8263af567e0d4b9b59675821f6522dc89f598
danielgronau@gmail.com <danielgronau@gmail.com@bdcd8b68-9dcc-aa74-f1b9-b4da3e61a8cc>
2013-02-09 16:21:15 +0000

    Java 8 changes

---
+++ branches/java8/src/main/java/org/highj/typeclass1/alternative/Alt.java
@@ -0,0 +1,18 @@
+package org.highj.typeclass1.alternative;
+
+import org.highj._;
+import org.highj.typeclass0.group.Semigroup;
+import org.highj.typeclass1.monad.Functor;
+
+public interface Alt<µ> extends Functor<µ> {
+
+    public <A> _<µ, A> mplus(_<µ, A> first, _<µ, A> second);
+
+    public default <A> Semigroup<_<µ, A>> asSemigroup() {
+        return Alt.this::mplus;
+    }
+
+    //public <F,A> _<F,List<A>> some(Applicative<F> applicative, _<F,A> fa);
+    //public <F,A> _<F,List<A>> many(Applicative<F> applicative, _<F,A> fa);
+
+}
