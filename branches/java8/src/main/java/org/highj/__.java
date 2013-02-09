d1515def8aebe1412b50f8f7689cf0a486a50080
danielgronau@gmail.com <danielgronau@gmail.com@bdcd8b68-9dcc-aa74-f1b9-b4da3e61a8cc>
2013-02-09 16:20:03 +0000

    Java 8 changes

--- branches/java8/src/main/java/org/highj/__.java
+++ branches/java8/src/main/java/org/highj/__.java
@@ -1,19 +1,6 @@
 package org.highj;
 
-import static org.highj.util.Contracts.require;
-
-public class __<µ, A, B> extends _<__.µ<µ, A>, B> {
-    public static class µ<µ, T> {
-        private µ() {
-        }
-    }
+public interface __<µ, A, B> extends _<__.µ<µ, A>, B> {
+    public static class µ<µ, T> {}
     
-    protected __(µ hidden) {
-        super(new __.µ<µ, A>());
-        require(hidden != null, "µ value can't be null");
-    }
-
-    public static <µ, A, B> __<µ, A, B> uncurry2(_<__.µ<µ, A>, B> curried) {
-        return (__<µ, A, B>) curried;
-    }
 }
