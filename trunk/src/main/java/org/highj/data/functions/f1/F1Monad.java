b4f7ddcedf0d3369c417a69f5fe65e07ddb7339e
daniel.gronau@gmail.com <daniel.gronau@gmail.com@bdcd8b68-9dcc-aa74-f1b9-b4da3e61a8cc>
2013-06-14 14:32:32 +0000

    extended F1Applicative to F1Monad (using the definition from Control.Monad.Reader)

--- trunk/src/main/java/org/highj/data/functions/f1/F1Monad.java
+++ trunk/src/main/java/org/highj/data/functions/f1/F1Monad.java
@@ -4,10 +4,11 @@ import org.highj._;
 import org.highj.__;
 import org.highj.data.functions.F1;
 import org.highj.typeclass1.monad.Applicative;
+import org.highj.typeclass1.monad.Monad;
 
 import java.util.function.Function;
 
-public class F1Applicative<R> implements Applicative<__.µ<F1.µ, R>> {
+public class F1Monad<R> implements Monad<__.µ<F1.µ, R>> {
 
     @Override
     public <A> F1<R, A> pure(A a) {
@@ -26,5 +27,11 @@ public class F1Applicative<R> implements Applicative<__.µ<F1.µ, R>> {
     @Override
     public <A, B> F1<R, B> map(Function<A, B> fAB, _<__.µ<F1.µ, R>, A> nestedA) {
         return F1.compose((F1<A, B>) fAB::apply, F1.narrow(nestedA));
+    }
+
+    @Override
+    public <A, B> F1<R, B> bind(_<__.µ<F1.µ, R>, A> a, Function<A, _<__.µ<F1.µ, R>, B>> fn) {
+        //m >>= k  = Reader $ \r -> runReader (k (runReader m r)) r
+        return r -> F1.narrow(fn.apply(F1.narrow(a).apply(r))).apply(r);
     }
 }
