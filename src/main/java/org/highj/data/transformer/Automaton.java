877b507d7d2712b7ff522c3d9c56d5488bb32709
Clinton Selke <clinuxrulz@gmail.com>
2015-07-10 18:18:05 +1000

    run method for Automaton

--- src/main/java/org/highj/data/transformer/Automaton.java
+++ src/main/java/org/highj/data/transformer/Automaton.java
@@ -7,6 +7,7 @@ package org.highj.data.transformer;
 
 import org.highj.__;
 import org.highj.___;
+import org.highj.data.collection.Stream;
 import org.highj.data.transformer.automaton.AutomatonArrow;
 import org.highj.data.transformer.automaton.AutomatonArrowChoice;
 import org.highj.data.transformer.automaton.AutomatonArrowPlus;
@@ -15,7 +16,9 @@ import org.highj.data.transformer.automaton.AutomatonArrowZero;
 import org.highj.data.transformer.automaton.AutomatonCategory;
 import org.highj.data.tuple.T2;
 import org.highj.typeclass2.arrow.Arrow;
+import org.highj.typeclass2.arrow.ArrowApply;
 import org.highj.typeclass2.arrow.ArrowChoice;
+import org.highj.typeclass2.arrow.ArrowLoop;
 import org.highj.typeclass2.arrow.ArrowPlus;
 import org.highj.typeclass2.arrow.ArrowZero;
 
@@ -36,6 +39,23 @@ public interface Automaton<A,B,C> extends ___<Automaton.µ, A, B, C> {
     }
     
     public __<A,B,T2<C,Automaton<A,B,C>>> unAutomaton();
+    
+    public static <A,E,B,C> __<A,T2<E,Stream<B>>,Stream<C>> run(ArrowLoop<A> aArrowLoop, ArrowApply<A> aArrowApply, Automaton<A,T2<E,B>,C> x) {
+        Arrow<A> a = aArrowApply;
+        return a.dot(
+            a.arr((T2<C,Stream<C>> x2) -> Stream.newStream(x2._1(), x2._2())),
+            a.dot(
+                a.second(aArrowApply.app()),
+                a.dot(
+                    a.arr((T2<T2<C,Automaton<A,T2<E,B>,C>>,T2<E,Stream<B>>> x2) -> T2.of(x2._1()._1(), T2.of(run(aArrowLoop, aArrowApply, x2._1()._2()), x2._2()))),
+                    a.dot(
+                        a.first(x.unAutomaton()),
+                        a.arr((T2<E,Stream<B>> x2) -> T2.of(T2.of(x2._1(), x2._2().head()), T2.of(x2._1(), x2._2().tail())))
+                    )
+                )
+            )
+        );
+    }
     
     public static <A> AutomatonCategory<A> automatonCategory(Arrow<A> aArrow) {
         return (AutomatonCategory<A>)() -> aArrow;
