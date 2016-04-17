88622c405cb88bb17ed24bc52b22d31bc9712602
DanielGronau <Daniel.Gronau@gmx.de>
2016-04-17 20:09:38 +0200

    Added JavaDoc and lift methods to Maybe

--- src/main/java/org/highj/typeclass1/comonad/Extend.java
+++ src/main/java/org/highj/typeclass1/comonad/Extend.java
@@ -9,18 +9,18 @@ import java.util.function.Function;
 //Minimal implementation: duplicate(_<W, A>) OR extend(Function<_<W, A>, B>)
 public interface Extend<W> extends Functor<W> {
 
-    public default <A> _<W, _<W, A>> duplicate(_<W, A> nestedA) {
+    default <A> _<W, _<W, A>> duplicate(_<W, A> nestedA) {
         // duplicate = extend id
         return extend(Function.<_<W, A>>identity()).apply(nestedA);
     }
 
-    public default <A, B> Function<_<W, A>, _<W, B>> extend(final Function<_<W, A>, B> fn) {
+    default <A, B> Function<_<W, A>, _<W, B>> extend(final Function<_<W, A>, B> fn) {
         //extend f = fmap f . duplicate
         return x -> lift(fn).apply(duplicate(x));
     }
 
     //(=>=)
-    public default <A, B, C> Function<_<W, A>, C> cokleisli(Function<_<W, A>, B> f, Function<_<W, B>, C> g) {
+    default <A, B, C> Function<_<W, A>, C> cokleisli(Function<_<W, A>, B> f, Function<_<W, B>, C> g) {
         //f =>= g = g . extend f
         return Functions.compose(g, extend(f));
     }
