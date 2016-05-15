d4f91ff15bbff4edeba780818ac1d0e43d355793
DanielGronau <Daniel.Gronau@gmx.de>
2016-05-15 13:12:39 +0200

    changed test case for IntMap, fails now

--- src/test/java/org/highj/data/collection/IntMapTest.java
+++ src/test/java/org/highj/data/collection/IntMapTest.java
@@ -6,6 +6,10 @@
 package org.highj.data.collection;
 
 import org.junit.Test;
+
+import java.util.Collections;
+import java.util.Random;
+
 import static org.assertj.core.api.Assertions.assertThat;
 
 /**
@@ -16,7 +20,9 @@ public class IntMapTest {
     @Test
     public void testIntMap() {
         IntMap<String> m = IntMap.empty();
-        for (int i = 0; i < 1000; ++i) {
+        java.util.List<Integer> ints = List.range(0,1,999).toJList();
+        Collections.shuffle(ints, new Random(123));
+        for (int i : ints) {
             m = m.insert(i, Integer.toHexString(i));
         }
         for (int i = 0; i < 1000; ++i) {
