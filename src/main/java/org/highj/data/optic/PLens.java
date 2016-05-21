fcf4d2df9d283d0446d54b1c85e8e9ea7fcf237d
DanielGronau <Daniel.Gronau@gmx.de>
2016-05-21 23:15:32 +0200

    #16 get JavaDoc working, WIP2

--- src/main/java/org/highj/data/optic/PLens.java
+++ src/main/java/org/highj/data/optic/PLens.java
@@ -10,7 +10,7 @@ import org.highj.typeclass0.group.Monoid;
 import org.highj.typeclass1.monad.Applicative;
 
 /**
- * A {@link PLens} can be seen as a pair of functions: - `get: S => A` i.e. from an `S`, we can extract an `A` - `set: (B, S) =>
+ * A {@link PLens} can be seen as a pair of functions: - `get: S =&gt; A` i.e. from an `S`, we can extract an `A` - `set: (B, S) =&gt;
  * T` i.e. if we replace an `A` by a `B` in an `S`, we obtain a `T`
  *
  * A {@link PLens} could also be defined as a weaker {@link PIso} where set requires an additional parameter than reverseGet.
@@ -21,7 +21,7 @@ import org.highj.typeclass1.monad.Applicative;
  * A {@link PLens} is also a valid {@link Getter}, {@link Fold}, {@link POptional}, {@link PTraversal} and {@link PSetter}
  *
  * Typically a {@link PLens} or {@link Lens} can be defined between a Product (e.g. case class, tuple, HList) and one of it is
- * component.
+ * components.
  *
  * @param <S> the source of a {@link PLens}
  * @param <T> the modified source of a {@link PLens}
@@ -34,43 +34,72 @@ public abstract class PLens<S, T, A, B> {
         super();
     }
 
-    /** get the target of a {@link PLens} */
+    /** Get the target of a {@link PLens}
+     * @param s the source value
+     * @return the target value
+     */
     public abstract A get(S s);
 
-    /** set polymorphically the target of a {@link PLens} using a function */
+    /** Set polymorphically the target of a {@link PLens} using a function
+     * @param b the modified target value
+     * @return a function to modify the source value
+     */
     public abstract F1<S, T> set(B b);
 
     /**
-     * modify polymorphically the target of a {@link PLens} with an Applicative function
+     * Modify polymorphically the target of a {@link PLens} with an Applicative function
+     * @param applicative the applicative functor
+     * @param f target modification function
+     * @param <X> applicative type
+     * @return result function
      */
     public abstract <X> F1<S, __<X, T>> modifyF(Applicative<X> applicative, Function<A, __<X, B>> f);
 
-    /** modify polymorphically the target of a {@link PLens} using a function */
+    /** Modify polymorphically the target of a {@link PLens} using a function
+     * @param f target modification function
+     * @return source modifying function
+     */
     public abstract F1<S, T> modify(final Function<A, B> f);
 
-    /** join two {@link PLens} with the same target */
+    /** Join two {@link PLens} with the same target
+     * @param other the second {@link PLens}
+     * @param <S1> the source type of the second {@link PLens}
+     * @param <T1> the modified source type of the second {@link PLens}
+     * @return the combined {@link PLens}
+     */
     public final <S1, T1> PLens<Either<S, S1>, Either<T, T1>, A, B> sum(final PLens<S1, T1, A, B> other) {
         return pLens(
                 e -> e.either(this::get, other::get),
                 b -> e -> e.bimap(PLens.this.set(b), other.set(b)));
     }
 
-    /***********************************************************/
-    /** Compose methods between a {@link PLens} and another Optics */
-    /***********************************************************/
+    /* **************************************************************/
+    /* * Compose methods between a {@link PLens} and another Optics */
+    /* **************************************************************/
 
-    /** compose a {@link PLens} with a {@link Fold} */
+    /** Compose a {@link PLens} with a {@link Fold}
+     * @param other the {@link Fold}
+     * @param <C> the target type of the {@link Fold}
+     * @return the composed {@link Fold}
+     */
     public final <C> Fold<S, C> composeFold(final Fold<A, C> other) {
         return asFold().composeFold(other);
     }
 
-    /** compose a {@link PLens} with a {@link Getter} */
+    /** Compose a {@link PLens} with a {@link Getter}
+     * @param other the {@link Getter}
+     * @param <C> the target type of the {@link Getter}
+     * @return the composed {@link Getter}
+     */
     public final <C> Getter<S, C> composeGetter(final Getter<A, C> other) {
         return asGetter().composeGetter(other);
     }
 
-    /**
-     * compose a {@link PLens} with a {@link PSetter}
+    /** Compose a {@link PLens} with a {@link PSetter}
+     * @param other the  {@link PSetter}
+     * @param <C> the target type of the {@link PSetter}
+     * @param <D> the modified target type of the {@link PSetter}
+     * @return the composed {@link PSetter}
      */
     public final <C, D> PSetter<S, T, C, D> composeSetter(final PSetter<A, B, C, D> other) {
         return asSetter().composeSetter(other);
@@ -78,22 +107,41 @@ public abstract class PLens<S, T, A, B> {
 
     /**
      * compose a {@link PLens} with a {@link PTraversal}
+     * @param other the  {@link PTraversal}
+     * @param <C> the target type of the {@link PTraversal}
+     * @param <D> the modified target type of the {@link PTraversal}
+     * @return the composed {@link PTraversal}
      */
     public final <C, D> PTraversal<S, T, C, D> composeTraversal(final PTraversal<A, B, C, D> other) {
         return asTraversal().composeTraversal(other);
     }
 
-    /** compose a {@link PLens} with an {@link POptional} */
+    /** Compose a {@link PLens} with an {@link POptional}
+     * @param other the  {@link POptional}
+     * @param <C> the target type of the {@link POptional}
+     * @param <D> the modified target type of the {@link POptional}
+     * @return the composed {@link POptional}
+     */
     public final <C, D> POptional<S, T, C, D> composeOptional(final POptional<A, B, C, D> other) {
         return asOptional().composeOptional(other);
     }
 
-    /** compose a {@link PLens} with a {@link PPrism} */
+    /** Compose a {@link PLens} with a {@link PPrism}
+     * @param other the  {@link PPrism}
+     * @param <C> the target type of the {@link PPrism}
+     * @param <D> the modified target type of the {@link PPrism}
+     * @return the composed {@link PPrism}
+     */
     public final <C, D> POptional<S, T, C, D> composePrism(final PPrism<A, B, C, D> other) {
         return asOptional().composeOptional(other.asOptional());
     }
 
-    /** compose a {@link PLens} with a {@link PLens} */
+    /** compose a {@link PLens} with a {@link PLens}
+     * @param other the second {@link PLens}
+     * @param <C> the target type of the second {@link PLens}
+     * @param <D> the modified target type of the second {@link PLens}
+     * @return the composed {@link PLens}
+     */
     public final <C, D> PLens<S, T, C, D> composeLens(final PLens<A, B, C, D> other) {
         final PLens<S, T, A, B> self = this;
         return new PLens<S, T, C, D>() {
@@ -119,16 +167,23 @@ public abstract class PLens<S, T, A, B> {
         };
     }
 
-    /** compose a {@link PLens} with an {@link PIso} */
+    /** Compose a {@link PLens} with an {@link PIso}
+     * @param other the  {@link PIso}
+     * @param <C> the target type of the {@link PIso}
+     * @param <D> the modified target type of the {@link PIso}
+     * @return the composed {@link PLens}
+     */
     public final <C, D> PLens<S, T, C, D> composeIso(final PIso<A, B, C, D> other) {
         return composeLens(other.asLens());
     }
 
-    /************************************************************************************************/
-    /** Transformation methods to view a {@link PLens} as another Optics */
-    /************************************************************************************************/
+    /* ********************************************************************/
+    /* * Transformation methods to view a {@link PLens} as another Optics */
+    /* ********************************************************************/
 
-    /** view a {@link PLens} as a {@link Fold} */
+    /** View a {@link PLens} as a {@link Fold}
+     * @return the {@link Fold}
+     */
     public final Fold<S, A> asFold() {
         return new Fold<S, A>() {
             @Override
@@ -138,7 +193,9 @@ public abstract class PLens<S, T, A, B> {
         };
     }
 
-    /** view a {@link PLens} as a {@link Getter} */
+    /** View a {@link PLens} as a {@link Getter}
+     * @return the {@link Getter}
+     */
     public final Getter<S, A> asGetter() {
         return new Getter<S, A>() {
             @Override
@@ -148,7 +205,9 @@ public abstract class PLens<S, T, A, B> {
         };
     }
 
-    /** view a {@link PLens} as a {@link PSetter} */
+    /** view a {@link PLens} as a {@link PSetter}
+     * @return the {@link PSetter}
+     */
     public PSetter<S, T, A, B> asSetter() {
         return new PSetter<S, T, A, B>() {
             @Override
@@ -163,7 +222,9 @@ public abstract class PLens<S, T, A, B> {
         };
     }
 
-    /** view a {@link PLens} as a {@link PTraversal} */
+    /** View a {@link PLens} as a {@link PTraversal}
+     * @return the {@link PTraversal}
+     */
     public PTraversal<S, T, A, B> asTraversal() {
         return new PTraversal<S, T, A, B>() {
 
@@ -175,7 +236,9 @@ public abstract class PLens<S, T, A, B> {
         };
     }
 
-    /** view a {@link PLens} as an {@link POptional} */
+    /** View a {@link PLens} as an {@link POptional}
+     * @return the {@link POptional}
+     */
     public POptional<S, T, A, B> asOptional() {
         final PLens<S, T, A, B> self = this;
         return new POptional<S, T, A, B>() {
@@ -210,8 +273,14 @@ public abstract class PLens<S, T, A, B> {
         return PIso.<S, T> pId().asLens();
     }
 
-    /**
-     * create a {@link PLens} using a pair of functions: one to get the target, one to set the target.
+    /** Create a {@link PLens} using a pair of functions: one to get the target, one to set the target.
+     * @param get the get function
+     * @param set the set function
+     * @param <S> the source type
+     * @param <T> the modified source type
+     * @param <A> the target type
+     * @param <B> the modified target type
+     * @return the {@link PLens}
      */
     public static <S, T, A, B> PLens<S, T, A, B> pLens(final Function<S, A> get, final Function<B, F1<S, T>> set) {
         return new PLens<S, T, A, B>() {
--- src/main/java/org/highj/data/optic/PPrism.java
+++ src/main/java/org/highj/data/optic/PPrism.java
@@ -10,7 +10,7 @@ import org.highj.typeclass0.group.Monoid;
 import org.highj.typeclass1.monad.Applicative;
 
 /**
- * A {@link PPrism} can be seen as a pair of functions: - `getOrModify: S => T \/ A` - `reverseGet : B => T`
+ * A {@link PPrism} can be seen as a pair of functions: - `getOrModify: S =&gt; T \/ A` - `reverseGet : B =&gt; T`
  *
  * A {@link PPrism} could also be defined as a weaker {@link PIso} where get can fail.
  *
@@ -33,92 +33,152 @@ public abstract class PPrism<S, T, A, B> {
         super();
     }
 
-    /** get the target of a {@link PPrism} or modify the source in case there is no target */
-
+    /** Get the target of a {@link PPrism} or modify the source in case there is no target
+     * @param s the source value
+     * @return the {@link Either}
+     */
     public abstract Either<T, A> getOrModify(S s);
 
-    /** get the modified source of a {@link PPrism} */
+    /** Get the modified source of a {@link PPrism}
+     * @param b the modified target value
+     * @return the modified source
+     */
     public abstract T reverseGet(B b);
 
-    /** get the target of a {@link PPrism} or nothing if there is no target */
+    /** Get the target of a {@link PPrism} or nothing if there is no target
+     * @param s the source value
+     * @return the {@link Maybe}
+     */
     public abstract Maybe<A> getMaybe(final S s);
 
-    /** modify polymorphically the target of a {@link PPrism} with an Applicative function */
+    /** Modify polymorphically the target of a {@link PPrism} with an Applicative function
+     * @param applicative the applicative functor
+     * @param f target modification function
+     * @param <X> applicative type
+     * @return result function
+     */
     public final <X> F1<S, __<X, T>> modifyF(final Applicative<X> applicative, final Function<A, __<X, B>> f) {
         return s -> getOrModify(s).either(
                 applicative::pure,
                 a -> applicative.map(this::reverseGet, f.apply(a))
                 );
-
     }
 
-    /** modify polymorphically the target of a {@link PPrism} with a function */
+    /** Modify polymorphically the target of a {@link PPrism} with a function
+     * @param f target modification function
+     * @return source modifying function
+     */
     public final F1<S, T> modify(final Function<A, B> f) {
         return s -> getOrModify(s).either(F1.id(), a -> reverseGet(f.apply(a)));
     }
 
-    /**
-     * modify polymorphically the target of a {@link PPrism} with a function. return empty if the {@link PPrism} is not matching
+    /** Modify polymorphically the target of a {@link PPrism} with a function. Return empty if the {@link PPrism} is not matching
+     * @param f the target modifying function
+     * @return the partial source modifying function
      */
     public final F1<S, Maybe<T>> modifyMaybe(final Function<A, B> f) {
         return s -> getMaybe(s).map(a -> reverseGet(f.apply(a)));
     }
 
-    /** set polymorphically the target of a {@link PPrism} with a value */
+    /** Set polymorphically the target of a {@link PPrism} with a value
+     * @param b the target value
+     * @return the source modifying function
+     */
     public final F1<S, T> set(final B b) {
         return modify(__ -> b);
     }
 
-    /** set polymorphically the target of a {@link PPrism} with a value. return empty if the {@link PPrism} is not matching */
+    /** Set polymorphically the target of a {@link PPrism} with a value. Return empty if the {@link PPrism} is not matching
+     * @param b the target value
+     * @return the partial, source modifying function
+     */
     public final F1<S, Maybe<T>> setMaybe(final B b) {
         return modifyMaybe(__ -> b);
     }
 
-    /** check if a {@link PPrism} has a target */
+    /** Check if a {@link PPrism} has a target
+     * @param s the modified source value
+     * @return true if matching
+     */
     public final boolean isMatching(final S s) {
         return getMaybe(s).isJust();
     }
 
-    /** create a {@link Getter} from the modified target to the modified source of a {@link PPrism} */
+    /** Create a {@link Getter} from the modified target to the modified source of a {@link PPrism}
+     * @return the {@link Getter}
+     */
     public final Getter<B, T> re() {
         return Getter.getter(this::reverseGet);
     }
 
-    /************************************************************/
-    /** Compose methods between a {@link PPrism} and another Optics */
-    /************************************************************/
+    /* ***************************************************************/
+    /* * Compose methods between a {@link PPrism} and another Optics */
+    /* ***************************************************************/
 
-    /** compose a {@link PPrism} with a {@link Fold} */
+    /** Compose a {@link PPrism} with a {@link Fold}
+     * @param other the {@link Fold}
+     * @param <C> the target type of the {@link Fold}
+     * @return the composed {@link Fold}
+     */
     public final <C> Fold<S, C> composeFold(final Fold<A, C> other) {
         return asFold().composeFold(other);
     }
 
-    /** compose a {@link PPrism} with a {@link Getter} */
+    /** Compose a {@link PPrism} with a {@link Getter}
+     * @param other the {@link Getter}
+     * @param <C> the target type of the {@link Getter}
+     * @return the composed {@link Fold}
+     */
     public final <C> Fold<S, C> composeGetter(final Getter<A, C> other) {
         return asFold().composeGetter(other);
     }
 
-    /** compose a {@link PPrism} with a {@link PSetter} */
+    /** Compose a {@link PPrism} with a {@link PSetter}
+     * @param other the {@link PSetter}
+     * @param <C> the target type of the {@link PSetter}
+     * @param <D> the modified target type of the {@link PSetter}
+     * @return the composed {@link PSetter}
+     */
     public final <C, D> PSetter<S, T, C, D> composeSetter(final PSetter<A, B, C, D> other) {
         return asSetter().composeSetter(other);
     }
 
-    /** compose a {@link PPrism} with a {@link PTraversal} */
+    /** Compose a {@link PPrism} with a {@link PTraversal}
+     * @param other the {@link PTraversal}
+     * @param <C> the target type of the {@link PTraversal}
+     * @param <D> the modified target type of the {@link PTraversal}
+     * @return the composed {@link PTraversal}
+     */
     public final <C, D> PTraversal<S, T, C, D> composeTraversal(final PTraversal<A, B, C, D> other) {
         return asTraversal().composeTraversal(other);
     }
 
-    /** compose a {@link PPrism} with a {@link POptional} */
+    /** Compose a {@link PPrism} with a {@link POptional}
+     * @param other the {@link POptional}
+     * @param <C> the target type of the {@link POptional}
+     * @param <D> the modified target type of the {@link POptional}
+     * @return the composed {@link POptional}
+     */
     public final <C, D> POptional<S, T, C, D> composeOptional(final POptional<A, B, C, D> other) {
         return asOptional().composeOptional(other);
     }
 
-    /** compose a {@link PPrism} with a {@link PLens} */
+    /** Compose a {@link PPrism} with a {@link PLens}
+     * @param other the {@link PLens}
+     * @param <C> the target type of the {@link PLens}
+     * @param <D> the modified target type of the {@link PLens}
+     * @return the composed {@link POptional}
+     */
     public final <C, D> POptional<S, T, C, D> composeLens(final PLens<A, B, C, D> other) {
         return asOptional().composeOptional(other.asOptional());
     }
 
-    /** compose a {@link PPrism} with a {@link PPrism} */
+    /** Compose a {@link PPrism} with a {@link PPrism}
+     * @param other the second {@link PPrism}
+     * @param <C> the target type of the second {@link PPrism}
+     * @param <D> the modified target type of the second {@link PPrism}
+     * @return the composed {@link PPrism}
+     */
     public final <C, D> PPrism<S, T, C, D> composePrism(final PPrism<A, B, C, D> other) {
         return new PPrism<S, T, C, D>() {
 
@@ -141,16 +201,23 @@ public abstract class PPrism<S, T, A, B> {
         };
     }
 
-    /** compose a {@link PPrism} with a {@link PIso} */
+    /** Compose a {@link PPrism} with a {@link PIso}
+     * @param other the {@link PIso}
+     * @param <C> the target type of the {@link PIso}
+     * @param <D> the modified target type of the {@link PIso}
+     * @return the composed {@link PPrism}
+     */
     public final <C, D> PPrism<S, T, C, D> composeIso(final PIso<A, B, C, D> other) {
         return composePrism(other.asPrism());
     }
 
-    /******************************************************************/
-    /** Transformation methods to view a {@link PPrism} as another Optics */
-    /******************************************************************/
+    /* *********************************************************************/
+    /* * Transformation methods to view a {@link PPrism} as another Optics */
+    /* *********************************************************************/
 
-    /** view a {@link PPrism} as a {@link Fold} */
+    /** View a {@link PPrism} as a {@link Fold}
+     * @return the {@link Fold}
+     */
     public final Fold<S, A> asFold() {
         return new Fold<S, A>() {
             @Override
@@ -160,7 +227,9 @@ public abstract class PPrism<S, T, A, B> {
         };
     }
 
-    /** view a {@link PPrism} as a {@link Setter} */
+    /** View a {@link PPrism} as a {@link PSetter}
+     * @return the {@link PSetter}
+     */
     public PSetter<S, T, A, B> asSetter() {
         return new PSetter<S, T, A, B>() {
             @Override
@@ -175,7 +244,9 @@ public abstract class PPrism<S, T, A, B> {
         };
     }
 
-    /** view a {@link PPrism} as a {@link PTraversal} */
+    /** View a {@link PPrism} as a {@link PTraversal}
+     * @return the {@link PTraversal}
+     */
     public PTraversal<S, T, A, B> asTraversal() {
         final PPrism<S, T, A, B> self = this;
         return new PTraversal<S, T, A, B>() {
@@ -188,7 +259,9 @@ public abstract class PPrism<S, T, A, B> {
         };
     }
 
-    /** view a {@link PPrism} as a {@link POptional} */
+    /** View a {@link PPrism} as a {@link POptional}
+     * @return the {@link POptional}
+     */
     public POptional<S, T, A, B> asOptional() {
         final PPrism<S, T, A, B> self = this;
         return new POptional<S, T, A, B>() {
@@ -225,7 +298,15 @@ public abstract class PPrism<S, T, A, B> {
         return PIso.<S, T> pId().asPrism();
     }
 
-    /** create a {@link PPrism} using the canonical functions: getOrModify and reverseGet */
+    /** Create a {@link PPrism} using the canonical functions: getOrModify and reverseGet
+     * @param getOrModify the getModify function
+     * @param reverseGet the reverseGet function
+     * @param <S> the source type
+     * @param <T> the modified source type
+     * @param <A> the target type
+     * @param <B> the modified target type
+     * @return the {@link PPrism}
+     */
     public static <S, T, A, B> PPrism<S, T, A, B> pPrism(final Function<S, Either<T, A>> getOrModify,
             final Function<B, T> reverseGet) {
         return new PPrism<S, T, A, B>() {
