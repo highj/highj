610a55e736997d3a54645d47d69527c86c53d6cc
DanielGronau <Daniel.Gronau@gmx.de>
2016-05-28 22:54:08 +0200

    HashMap based on IntMap implementation

---
+++ src/main/java/org/highj/data/HashMap.java
@@ -0,0 +1,252 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.highj.data;
+
+import org.derive4j.hkt.__;
+import org.highj.data.coroutine.ProducerT;
+import org.highj.data.tuple.T0;
+import org.highj.data.tuple.T1;
+import org.highj.data.tuple.T2;
+import org.highj.function.Strings;
+
+import java.util.Arrays;
+import java.util.Iterator;
+
+public class HashMap<K, V> implements Iterable<T2<K, V>> {
+    private static final int NUM_BRANCHING_BITS = 5;
+    private static final int NUM_BRANCHES = 1 << NUM_BRANCHING_BITS;
+    private static final int MASK = (1 << NUM_BRANCHING_BITS) - 1;
+    private static final Node<?, ?>[] EMPTY_ARRAY = new Node<?, ?>[NUM_BRANCHES];
+
+    static {
+        Arrays.fill(EMPTY_ARRAY, Leaf.empty());
+    }
+
+    private final Node<K, V> root;
+
+    private HashMap(Node<K, V> root) {
+        this.root = root;
+    }
+
+    private static final HashMap<?, ?> EMPTY = new HashMap<>(Leaf.empty());
+
+    @SuppressWarnings("unchecked")
+    public static <K, V> HashMap<K, V> empty() {
+        return (HashMap<K, V>) EMPTY;
+    }
+
+    public boolean isEmpty() {
+        return root.isEmpty();
+    }
+
+    @Override
+    public Iterator<T2<K, V>> iterator() {
+        return ProducerT.toIterator(ProducerT.narrow(root.generator(0, 0)));
+    }
+
+    public int size() {
+        return root.size();
+    }
+
+    public HashMap<K, V> insert(K key, V value) {
+        return new HashMap<>(root.insert(key.hashCode(), key, value));
+    }
+
+    public Maybe<V> lookup(K key) {
+        return root.lookup(key.hashCode(), key);
+    }
+
+    public HashMap<K, V> delete(K key) {
+        Node<K, V> root2 = root.delete(key.hashCode(), key);
+        return root2 == root ? this : new HashMap<>(root2);
+    }
+
+    @Override
+    public String toString() {
+        return root.toString();
+    }
+
+    private interface Node<K, V> {
+
+        boolean isEmpty();
+
+        int size();
+
+        Node<K, V> insert(int hash, K key, V value);
+
+        Maybe<V> lookup(int hash, K key);
+
+        Node<K, V> delete(int hash, K key);
+
+        __<__<__<ProducerT.µ, T2<K, V>>, T1.µ>, T0> generator(int shift, int hash);
+    }
+
+    private static class Leaf<K, V> implements Node<K, V> {
+
+        private static final Leaf<?, ?> EMPTY = new Leaf<>(List.of());
+
+        @SuppressWarnings("unchecked")
+        public static <K, V> Leaf<K, V> empty() {
+            return (Leaf<K, V>) EMPTY;
+        }
+
+        private final List<T2<K, V>> bucket;
+
+        private Leaf(K key, V value) {
+            this.bucket = List.of(T2.of(key, value));
+        }
+
+        private Leaf(List<T2<K, V>> bucket) {
+            this.bucket = bucket;
+        }
+
+        @Override
+        public boolean isEmpty() {
+            return bucket.isEmpty();
+        }
+
+        @Override
+        public int size() {
+            return bucket.size();
+        }
+
+        @Override
+        public Node<K, V> insert(int hash, K key, V value) {
+            if (hash == 0) {
+                return new Leaf<>(bucket.filter(p -> !p._1().equals(key)).plus(T2.of(key, value)));
+            } else {
+                int newHash = hash >>> NUM_BRANCHING_BITS;
+                int index = hash & MASK;
+                Node<K, V>[] nodes = emptyArray();
+                if (newHash == 0) {
+                    nodes[0] = this;
+                    nodes[index] = new Leaf<>(key, value);
+                } else {
+                    nodes[index] = Leaf.<K, V>empty().insert(newHash, key, value);
+                    if (index != 0) {
+                        nodes[0] = this;
+                    } else {
+                        bucket.forEach(t2 -> nodes[0] = nodes[0].insert(0, t2._1(), t2._2()));
+                    }
+                }
+                return new Branch<>(nodes);
+            }
+        }
+
+        @Override
+        public Maybe<V> lookup(int hash, K key) {
+            return (hash == 0)
+                    ? bucket.filter(p -> p._1().equals(key)).maybeHead().map(T2::_2)
+                    : Maybe.Nothing();
+        }
+
+        @Override
+        public String toString() {
+            return "(Leaf " + Strings.mkString("", "", "", bucket) + ")";
+        }
+
+        @Override
+        public Node<K, V> delete(int hash, K key) {
+            if (hash != 0) {
+                return this;
+            }
+            List<T2<K, V>> newBucket = bucket.filter(p -> !p._1().equals(key));
+            return newBucket.isEmpty() ? Leaf.empty() : new Leaf<>(newBucket);
+        }
+
+        @Override
+        public __<__<__<ProducerT.µ, T2<K, V>>, T1.µ>, T0> generator(int shift, int hash) {
+            return ProducerT.<T2<K, V>, T1.µ>monad().sequence_(bucket.map(ProducerT::yield));
+        }
+    }
+
+    private static class Branch<K, V> implements Node<K, V> {
+        private final Node<K, V>[] nodes;
+
+        private Branch(Node<K, V>[] nodes) {
+            this.nodes = nodes;
+        }
+
+        @Override
+        public boolean isEmpty() {
+            for (Node<K, V> node : nodes) {
+                if (!node.isEmpty()) {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        @Override
+        public int size() {
+            int size = 0;
+            for (Node<K, V> node : nodes) {
+                size += node.size();
+            }
+            return size;
+        }
+
+        @Override
+        public Node<K, V> insert(int hash, K key, V value) {
+            int newHash = hash >>> NUM_BRANCHING_BITS;
+            int index = hash & MASK;
+            Node<K, V>[] newNodes = Arrays.copyOf(nodes, nodes.length);
+            newNodes[index] = nodes[index].insert(newHash, key, value);
+            return new Branch<>(newNodes);
+        }
+
+        @Override
+        public Maybe<V> lookup(int hash, K key) {
+            int newHash = hash >>> NUM_BRANCHING_BITS;
+            int index = hash & MASK;
+            return nodes[index].lookup(newHash, key);
+        }
+
+        @Override
+        public Node<K, V> delete(int hash, K key) {
+            int newHash = hash >>> NUM_BRANCHING_BITS;
+            int index = hash & MASK;
+            Node<K, V> node = nodes[index];
+            if (node.isEmpty()) {
+                return this;
+            } else {
+                Node<K, V> newNode = node.delete(newHash, key);
+                if (newNode == node) {
+                    return this;
+                } else {
+                    Node<K, V>[] newNodes = Arrays.copyOf(nodes, nodes.length);
+                    newNodes[index] = newNode;
+                    Node<K, V> branch = new Branch<>(newNodes);
+                    return branch.isEmpty() ? Leaf.empty() : branch;
+                }
+            }
+        }
+
+        @Override
+        public String toString() {
+            return Strings.mkString("(Branch ", " ", ")", nodes);
+        }
+
+        @Override
+        public __<__<__<ProducerT.µ, T2<K, V>>, T1.µ>, T0> generator(int shift, int hash) {
+            return ProducerT.<T2<K, V>, T1.µ>monad().sequence_(
+                    List.zip(
+                            List.of(nodes),
+                            List.range(0)
+                    ).map((T2<Node<K, V>, Integer> x) -> {
+                        int newShift = shift + NUM_BRANCHING_BITS;
+                        int newHash = hash | (x._2() << shift);
+                        return x._1().generator(newShift, newHash);
+                    })
+            );
+        }
+    }
+
+    @SuppressWarnings("unchecked")
+    private static <K, V> Node<K, V>[] emptyArray() {
+        return Arrays.copyOf((Node<K, V>[]) EMPTY_ARRAY, NUM_BRANCHES);
+    }
+}
---
+++ src/test/java/org/highj/data/HashMapTest.java
@@ -0,0 +1,127 @@
+package org.highj.data;
+
+import org.highj.data.tuple.T2;
+import org.junit.Test;
+
+import java.util.Collections;
+import java.util.Iterator;
+import java.util.Random;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class HashMapTest {
+    @Test
+    public void testEmpty() {
+        HashMap<String, Integer> emptyMap = HashMap.empty();
+        assertThat(emptyMap).isEmpty();
+        assertThat(emptyMap.isEmpty()).isTrue();
+    }
+
+    @Test
+    public void isEmpty() {
+        assertThat(HashMap.empty().isEmpty()).isTrue();
+        assertThat(HashMap.empty().insert("one",1).isEmpty()).isFalse();
+        assertThat(HashMap.empty().insert("one",1).delete("one").isEmpty()).isTrue();
+    }
+
+    @Test
+    public void testIterator() {
+        HashMap<String,Integer> m = HashMap.empty();
+        java.util.List<Integer> ints = List.range(0,1,999).toJList();
+        Collections.shuffle(ints, new Random(123));
+        for (int i : ints) {
+            m = m.insert(Integer.toHexString(i), i);
+        }
+        Iterator<T2<String,Integer>> iterator = m.iterator();
+        int count = 0;
+        while (iterator.hasNext()) {
+            count++;
+            T2<String,Integer> x = iterator.next();
+            assertThat(Integer.toHexString(x._2())).isEqualTo(x._1());
+        }
+        assertThat(count).isEqualTo(1000);
+    }
+
+    @Test
+    public void testSize() {
+        assertThat(HashMap.empty().size()).isEqualTo(0);
+        HashMap<String, Integer> map = HashMap.<String, Integer>empty().insert("one", 1).insert("two", 2).insert("three", 3);
+        assertThat(map.size()).isEqualTo(3);
+
+        HashMap<String,Integer> m = HashMap.empty();
+        java.util.List<Integer> ints = List.range(0,1,999).toJList();
+        Collections.shuffle(ints, new Random(123));
+        for (int i : ints) {
+            m = m.insert(Integer.toHexString(i), i);
+        }
+        assertThat(m.size()).isEqualTo(1000);
+    }
+
+    @Test
+    public void testInsert() {
+        HashMap<String, Integer> map = HashMap.<String, Integer>empty()
+                .insert("one", 1).insert("two", 2).insert("three", 3).insert("one",100).insert("four",4);
+        assertThat(map.lookup("one").get()).isEqualTo(100);
+    }
+
+    @Test
+    public void testLookup() {
+        HashMap<String, Integer> map = HashMap.<String, Integer>empty().insert("one", 1).insert("two", 2).insert("three", 3);
+        assertThat(map.lookup("two").get()).isEqualTo(2);
+        assertThat(map.lookup("four").isNothing());
+    }
+
+    @Test
+    public void testDelete() {
+        HashMap<String, Integer> map = HashMap.<String, Integer>empty()
+                .insert("one", 1).insert("two", 2).insert("three", 3)
+                .delete("four").delete("two");
+        assertThat(map.lookup("one").isJust()).isTrue();
+        assertThat(map.lookup("two").isNothing()).isTrue();
+        assertThat(map.lookup("three").isJust()).isTrue();
+    }
+
+    @Test
+    public void testToString() {
+        HashMap<Object, Object> map = HashMap.empty().insert("one", 1).insert("two", 2).insert("three", 3);
+        assertThat(map.toString()).contains("Leaf (one,1)");
+        assertThat(map.toString()).contains("Leaf (two,2)");
+        assertThat(map.toString()).contains("Leaf (three,3)");
+    }
+
+    @Test
+    public void testCollisions() {
+        HashMap<HashCollider, Integer> map = HashMap.<HashCollider,Integer>empty()
+                .insert(new HashCollider("one"), 1)
+                .insert(new HashCollider("two"), 2)
+                .insert(new HashCollider("three"), 3);
+        assertThat(map.size()).isEqualTo(3);
+        assertThat(map.lookup(new HashCollider("one")).get()).isEqualTo(1);
+        assertThat(map.lookup(new HashCollider("two")).get()).isEqualTo(2);
+        assertThat(map.lookup(new HashCollider("three")).get()).isEqualTo(3);
+    }
+
+    private static class HashCollider {
+        public final String key;
+
+        private HashCollider(String key) {
+            this.key = key;
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            return key.equals(((HashCollider) o).key);
+        }
+
+        @Override
+        public int hashCode() {
+            return 11;
+        }
+
+        @Override
+        public String toString() {
+            return "#" + key;
+        }
+    }
+
+}
