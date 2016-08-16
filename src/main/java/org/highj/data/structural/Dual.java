a6f1476dd4693cc9cfa276b1b7770ba434c786eb
DanielGronau <Daniel.Gronau@gmx.de>
2016-08-16 22:36:13 +0200

    improved Dual

--- src/main/java/org/highj/data/structural/Dual.java
+++ src/main/java/org/highj/data/structural/Dual.java
@@ -4,11 +4,15 @@ import org.derive4j.hkt.__;
 import org.derive4j.hkt.__2;
 import org.derive4j.hkt.__3;
 import org.highj.data.structural.dual.DualCategory;
+import org.highj.typeclass0.group.Group;
+import org.highj.typeclass0.group.Monoid;
+import org.highj.typeclass0.group.Semigroup;
 import org.highj.typeclass2.arrow.Category;
 
 public class Dual<M, A, B> implements __3<Dual.µ, M, A, B> {
 
-    public static class µ {}
+    public interface µ {
+    }
 
     private final __2<M, B, A> value;
 
@@ -25,8 +29,23 @@ public class Dual<M, A, B> implements __3<Dual.µ, M, A, B> {
         return value;
     }
 
-    public static <M,A,B> __2<M, B, A> get(__<__<__<µ, M>, A>, B> dual) {
+    public static <M, A, B> __2<M, B, A> get(__<__<__<µ, M>, A>, B> dual) {
         return narrow(dual).get();
+    }
+
+    public static <M, A, B> Semigroup<__<__<__<µ, M>, A>, B>> semigroup(Semigroup<__2<M, B, A>> mSemigroup) {
+        return (x, y) -> new Dual<>(mSemigroup.apply(Dual.get(x), Dual.get(y)));
+    }
+
+    public static <M, A, B> Monoid<__<__<__<µ, M>, A>, B>> monoid(Monoid<__2<M, B, A>> mMonoid) {
+        return Monoid.create(new Dual<>(mMonoid.identity()),
+                (x, y) -> new Dual<>(mMonoid.apply(Dual.get(x), Dual.get(y))));
+    }
+
+    public static <M, A, B> Group<__<__<__<µ, M>, A>, B>> group(Group<__2<M, B, A>> mGroup) {
+        return Group.create(new Dual<>(mGroup.identity()),
+                (x, y) -> new Dual<>(mGroup.apply(Dual.get(x), Dual.get(y))),
+                z -> new Dual<>(mGroup.inverse(Dual.get(z))));
     }
 
     public static <M> DualCategory<M> category(final Category<M> category) {
