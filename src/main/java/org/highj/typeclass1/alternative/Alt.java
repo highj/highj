8bd68e802e11415ad69e7ea648b127fcff6d8caf
Clinton Selke <clinuxrulz@gmail.com>
2017-12-14 23:49:50 +1000

    Alt::some() and Alt::many()

--- src/main/java/org/highj/typeclass1/alternative/Alt.java
+++ src/main/java/org/highj/typeclass1/alternative/Alt.java
@@ -1,18 +1,31 @@
 package org.highj.typeclass1.alternative;
 
 import org.derive4j.hkt.__;
+import org.highj.data.List;
+import org.highj.data.tuple.T1;
+import org.highj.typeclass1.LazifyH;
 import org.highj.typeclass0.group.Semigroup;
 import org.highj.typeclass1.functor.Functor;
+import org.highj.typeclass1.monad.Applicative;
 
 public interface Alt<F> extends Functor<F> {
 
-    public <A> __<F, A> mplus(__<F, A> first, __<F, A> second);
+    <A> __<F, A> mplus(__<F, A> first, __<F, A> second);
 
-    public default <A> Semigroup<__<F, A>> asSemigroup() {
+    default <A> Semigroup<__<F, A>> asSemigroup() {
         return Alt.this::mplus;
     }
 
-    //public <F,A> __<F,List<A>> some(Applicative<F> applicative, __<F,A> fa);
-    //public <F,A> __<F,List<A>> many(Applicative<F> applicative, __<F,A> fa);
+    default <A> __<F,List<A>> some(Applicative<F> applicative, LazifyH<F> lazify, __<F,A> fa) {
+        return applicative.apply2(
+            (A head) -> (List<A> tail) -> List.Cons(head, tail),
+            fa,
+            lazify.lazifyH(T1.of$(() -> many(applicative, lazify, fa)))
+        );
+    }
+
+    default <A> __<F,List<A>> many(Applicative<F> applicative, LazifyH<F> lazify, __<F,A> fa) {
+        return mplus(some(applicative, lazify, fa), applicative.pure(List.Nil()));
+    }
 
 }
