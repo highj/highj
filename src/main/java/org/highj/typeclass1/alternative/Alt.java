c1b55a52dd6beacd38a0fbf88386844a1ea59378
Clinton Selke <clinuxrulz@gmail.com>
2017-12-15 12:19:07 +1000

    Alt::some(), Alt::many() making use of fix point combinator

--- src/main/java/org/highj/typeclass1/alternative/Alt.java
+++ src/main/java/org/highj/typeclass1/alternative/Alt.java
@@ -2,7 +2,6 @@ package org.highj.typeclass1.alternative;
 
 import org.derive4j.hkt.__;
 import org.highj.data.List;
-import org.highj.data.tuple.T1;
 import org.highj.typeclass1.LazifyH;
 import org.highj.typeclass0.group.Semigroup;
 import org.highj.typeclass1.functor.Functor;
@@ -17,15 +16,11 @@ public interface Alt<F> extends Functor<F> {
     }
 
     default <A> __<F,List<A>> some(Applicative<F> applicative, LazifyH<F> lazify, __<F,A> fa) {
-        return applicative.apply2(
-            (A head) -> (List<A> tail) -> List.Cons(head, tail),
-            fa,
-            lazify.lazifyH(T1.of$(() -> many(applicative, lazify, fa)))
-        );
+        return AltUtil.some_many(this, applicative, lazify, fa)._1();
     }
 
     default <A> __<F,List<A>> many(Applicative<F> applicative, LazifyH<F> lazify, __<F,A> fa) {
-        return mplus(some(applicative, lazify, fa), applicative.pure(List.Nil()));
+        return AltUtil.some_many(this, applicative, lazify, fa)._2();
     }
 
 }
