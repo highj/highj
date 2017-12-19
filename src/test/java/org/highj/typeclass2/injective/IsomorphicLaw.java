6851719ae8940d7d4111279153e65a8901bac8b8
DanielGronau <Daniel.Gronau@gmx.de>
2017-12-20 00:00:15 +0100

    injective and isomorphic laws, and an Isomorphic1 example

---
+++ src/test/java/org/highj/typeclass2/injective/IsomorphicLaw.java
@@ -0,0 +1,39 @@
+package org.highj.typeclass2.injective;
+
+import org.highj.data.eq.Eq;
+import org.highj.util.Gen;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class IsomorphicLaw<A,B> extends InjectiveLaw<A,B> {
+
+    private final Isomorphic<A,B> isomorphic;
+    protected final Gen<B> genB;
+
+    public IsomorphicLaw(Isomorphic<A,B> isomorphic, Gen<A> genA, Gen<B> genB, Eq<A> eqA, Eq<B> eqB) {
+        super(isomorphic, genA, eqA, eqB);
+        this.isomorphic = isomorphic;
+        this.genB = genB;
+    }
+
+    public void isomorphism() {
+        for(A a : genA.get(20)) {
+            assertThat(eqA.eq(a, isomorphic.from(isomorphic.to(a)))).isTrue();
+        }
+        for(B b : genB.get(20)) {
+            assertThat(eqB.eq(b, isomorphic.to(isomorphic.from(b)))).isTrue();
+        }
+    }
+
+    public void inverseInjectivity() {
+        new InjectiveLaw<>(isomorphic.inverse(), genB, eqB, eqA).test();
+    }
+
+    @Override
+    public void test() {
+        isomorphism();
+        inverseInjectivity();
+        super.test();
+    }
+
+}
