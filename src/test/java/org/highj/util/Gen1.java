05f8bbb8cdbcb87b578347d3a2522ff66270a689
DanielGronau <Daniel.Gronau@gmx.de>
2018-01-24 21:03:06 +0100

    introduced Gen2 generator

--- src/test/java/org/highj/util/Gen1.java
+++ src/test/java/org/highj/util/Gen1.java
@@ -3,6 +3,7 @@ package org.highj.util;
 import org.derive4j.hkt.__;
 import org.highj.data.List;
 import org.highj.data.Maybe;
+import org.highj.data.tuple.T1;
 
 /**
  * A generator for testing type constructors.
@@ -25,6 +26,13 @@ public interface Gen1<F> {
         public <T> Gen<__<List.µ, T>> gen(Gen<T> gen) {
             return maxSize -> List.range(0, 1, maxSize-1).map(i ->
                     List.fromIterable(gen.get(i)));
+        }
+    };
+
+    Gen1<T1.µ> t1Gen1 = new Gen1<T1.µ>() {
+        @Override
+        public <T> Gen<__<T1.µ, T>> gen(Gen<T> gen) {
+            return gen.map(T1::of);
         }
     };
 }
---
+++ src/test/java/org/highj/util/Gen2.java
@@ -0,0 +1,30 @@
+package org.highj.util;
+
+import org.derive4j.hkt.__2;
+import org.highj.data.Either;
+import org.highj.data.tuple.T2;
+
+/**
+ * A generator for testing type constructors.
+ *
+ * @param <F> the type constuctor type
+ */
+public interface Gen2<F> {
+    <T, U> Gen<__2<F, T, U>> gen(Gen<T> gen1, Gen<U> gen2);
+
+    Gen2<T2.µ> t2Gen2 = new Gen2<T2.µ>() {
+        @Override
+        public <T, U> Gen<__2<T2.µ, T, U>> gen(Gen<T> gen1, Gen<U> gen2) {
+            return Gen.zip(gen1, gen2).map(t2 -> t2);
+        }
+    };
+
+    Gen2<Either.µ> eitherGen2 = new Gen2<Either.µ>() {
+        @Override
+        public <T, U> Gen<__2<Either.µ, T, U>> gen(Gen<T> gen1, Gen<U> gen2) {
+            return Gen.zip(gen1, gen2).map(
+                    t2 -> Gen.rnd.nextBoolean() ? Either.Left(t2._1()) : Either.Right(t2._2()));
+        }
+    };
+
+}
