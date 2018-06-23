f9aeecde3d1d1b1eb99ea5f24b77c2794fe2db19
Clinton Selke <clinuxrulz@gmail.com>
2018-06-23 21:02:19 +1000

    lam with Cast. The Cast instance will always need to be explicit in Java, only in Haskell it can be Implicit. So tiny bit ugly to use, but could be used as a primitive to make bigger lambdas.

--- src/main/java/org/highj/typeclass2/category/Lam2CCC.java
+++ src/main/java/org/highj/typeclass2/category/Lam2CCC.java
@@ -17,16 +17,6 @@ public class Lam2CCC<K,Tensor,Hom,Unit> {
         return new Lam2CCC<>(ccc);
     }
 
-    /*
-    lam :: forall k i a b. CCC k => ((forall x. Cast k x (Tensor k i a) => k x a) -> k (Tensor k i a) b) -> k i (Hom k a b)
-    lam f = curry (f exr_) where
-      exr_ :: forall x. Cast k x (Tensor k i a) => k x a
-      exr_ = exr . (cast :: k x (Tensor k i a))
-    */
-    /* ??? (Requires Cast but if we use it, then the end-user needs to keep supplying it. It would not be implicit).
-    public <I,X,A,B> __2<K,I,__3<Hom,K,A,B>> lam(F1<__2<K,__3<X,A>,__2<K,__3<Tensor,K,I,A>,B>> f) {}
-    */
-
     public <I,A,B> __2<K,I,__3<Hom,K,A,B>> lam1(F1<__2<K,__3<Tensor,K,I,A>,A>,__2<K,__3<Tensor,K,I,A>,B>> f) {
         return ccc.curry(f.apply(ccc.exr()));
     }
@@ -45,6 +35,14 @@ public class Lam2CCC<K,Tensor,Hom,Unit> {
 
     public <I,A,B,C,D> __2<K,I,__3<Hom,K,A,__3<Hom,K,B,__3<Hom,K,C,D>>>> lam3Curried(F1<__2<K,__3<Tensor,K,__3<Tensor,K,__3<Tensor,K,I,A>,B>,C>,A>,F1<__2<K,__3<Tensor,K,__3<Tensor,K,__3<Tensor,K,I,A>,B>,C>,B>,F1<__2<K,__3<Tensor,K,__3<Tensor,K,__3<Tensor,K,I,A>,B>,C>,C>,__2<K,__3<Tensor,K,__3<Tensor,K,__3<Tensor,K,I,A>,B>,C>,D>>>> f) {
         return lam3((x, y, z) -> f.apply(x).apply(y).apply(z));
+    }
+
+    // lam :: forall k i a b. CCC k => ((forall x. Cast k x (Tensor k i a) => k x a) -> k (Tensor k i a) b) -> k i (Hom k a b)
+    // lam f = curry (f exr_) where
+    //   exr_ :: forall x. Cast k x (Tensor k i a) => k x a
+    //   exr_ = exr . (cast :: k x (Tensor k i a))
+    public <X,I,A,B> __2<K,I,__3<Hom,K,A,B>> lam(__2<K,X,__3<Tensor,K,I,A>> cast, F1<__2<K,X,A>,__2<K,__3<Tensor,K,I,A>,B>> f) {
+        return ccc.curry(f.apply(ccc.dot(ccc.exr(),cast)));
     }
 
     private void test() {
